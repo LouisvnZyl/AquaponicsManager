d92346db5eff8cfd0f0f9c4c3ad921928af2b0b4
Louis van Zyl <louis.vanzyl@entelect.co.za>
2022-04-15 11:41:43 +0200

    Added needed configurations

--- WebHost/AquaponicsManager.WebHost/AquaponicsManager.WebHost/Startup.cs
+++ WebHost/AquaponicsManager.WebHost/AquaponicsManager.WebHost/Startup.cs
@@ -1,4 +1,10 @@
-﻿namespace AquaponicsManager.WebHost
+﻿using AquaponicsManager.WebHost.Constants;
+using AquaponicsManager.WebHost.Helpers;
+using AquaponicsManager.WebHost.Models.ConfigurationMappers;
+using AquaponicsManager.WebHost.ServiceExtentions;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+namespace AquaponicsManager.WebHost
 {
     public class Startup
     {
@@ -8,5 +14,29 @@
         }
 
         public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAppsettingService(Configuration);
+            //services.AddDataAccessServices(Configuration);
+            //services.AddApplicationServices(Configuration);
+
+            var apiConfigurationSettings = AppsettingsMapper.GetMappedValues<ApiCorsConfiguration>(AppSettingsConfigurationSections.ApiCorsConfigurationSectionName, Configuration);
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: ApiConstants.CorsOriginPolicyName,
+                    new CorsPolicyBuilder()
+                    .WithHeaders(apiConfigurationSettings.AllowedHeaders)
+                    .WithMethods(apiConfigurationSettings.AllowedMethods)
+                    .AllowAnyOrigin().
+                    Build());
+            });
+
+            services.AddControllers();
+            services.AddSwaggerWithXml();
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
+        }
     }
 }
