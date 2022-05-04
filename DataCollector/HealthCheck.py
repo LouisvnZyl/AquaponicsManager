fd65c47d116c4c7a65c342e42838c231d1662991
Louis van Zyl <louis.vanzyl@entelect.co.za>
2022-05-04 18:02:52 +0200

    Add basic pyton requests for pi

--- DataCollector/HealthCheck.py
+++ DataCollector/HealthCheck.py
@@ -1,5 +1,20 @@
 import requests
+import time
+import RPi.GPIO as GPIO
 
-response = requests.get('https://localhost:5001/api/health', verify=False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(14,GPIO.OUT)
 
-print(response.json())
+while(True):
+    GPIO.output(14, GPIO.HIGH)
+    getResponse = requests.get('http://192.168.1.109:45457/api/health', verify = False)
+    print(getResponse.json())
+    
+    mydata={}
+    mydata['id'] = "87440"
+    postResponse = requests.post('http://192.168.1.109:45457/api/health/1', verify = False)
+    print(postResponse.text)
+    
+    GPIO.output(14, GPIO.LOW)
+    time.sleep(1)
